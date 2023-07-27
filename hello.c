d01a07e6215a36e230d29153102a566efa5e3a48
Patrick <p.grant012@gmail.com>
2023-07-26 21:23:19 -0400

    add sum function to hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+int sum (int x, int y) {
+  return x+y;
+}
+
 int main(){
-  printf("Hello World");
+  printf("Hello World\n");
+
+  printf("Sum of 5 + 6: %d\n", sum(5,6));
 
   return 0;
 }

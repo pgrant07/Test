967bc1fdc81ab774bc90e5ecef4ce61a4993e81a
pgrant07 <p.grant012@gmail.com>
2023-07-28 21:27:22 -0400

    add diff function to hello.c

--- hello.c
+++ hello.c
@@ -4,10 +4,15 @@ int sum (int x, int y) {
   return x+y;
 }
 
+int diff (int x, int y) {
+  return x-y;
+}
+
 int main(){
   printf("Hello World\n");
 
   printf("Sum of 5 + 6: %d\n", sum(5,6));
+  printf("Difference of 11 - 6: %d\n", diff(11,6));
 
   return 0;
 }
